5e3b1b20a65abb80d57f7299c864bdf14fb64c3f
Thai Nguyen <nguyenxuanthai7@gmail.com>
2026-01-01 14:26:03 +0700

    init BE

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ENV.FRONTEND_URL,
+  })
+);
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port: ${ENV.PORT}`);
+});
